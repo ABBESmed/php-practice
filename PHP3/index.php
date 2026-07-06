6c9cdb7313a37d80bf06c51779f1d6b0801e27d5
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-06 10:24:34 +0200

    PHP arithmetic

---
+++ PHP3/index.php
@@ -0,0 +1,51 @@
+<?php
+    // PHP arithmetic
+    // variables declaration and assignment
+    $x = 10;
+    $y = 2;
+    $z = null;
+    $t = null;
+    $w = null;
+    $i = null;
+    $m = null;
+    $n = null;
+
+    // calculations 
+    $z = $x + $y;
+    $t = $x - $y;
+    $w = $x * $y;
+    $i = $x / $y;
+    $m = $x ** $y;
+    $n = $x % $y;
+
+    // display results
+    echo "add: " . $z . "<br />";
+    echo "sub: " . $t . "<br />";
+    echo "mul: " . $w . "<br />";
+    echo "div: " . $i . "<br />";
+    echo "pow: " . $m . "<br />";
+    echo "mod: " . $n . "<br />";
+
+    // Increment / Decrement operators
+    $counter = 0;
+    $counter++; // <=> $counter = $counter + 1;
+    echo $counter . "<br />";
+
+    $counter--; // <=> $counter = $counter - 1;
+    echo $counter . "<br />";
+
+    $counter+=2;
+    echo $counter . "<br />";
+
+
+
+
+
+
+
+
+
+
+
+
+?>
