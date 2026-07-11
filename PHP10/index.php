5c6c76571fc1d25a5bf1a62b7d17ba2a17ba5101
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-12 00:20:15 +0200

    while loop

---
+++ PHP10/index.php
@@ -0,0 +1,18 @@
+<?php
+// PHP while loops explained = do some code infinitely while some condtion remains true
+$counter = 0;
+while($counter <= 10){
+    $counter++;
+    echo $counter . "<br />";
+}
+
+
+
+
+
+
+
+
+
+
+?>
