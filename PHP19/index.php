a1befc4241169932d65805d4023ee185a14fc348
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-15 06:16:12 +0200

    PHP include() function explained

---
+++ PHP19/index.php
@@ -0,0 +1,21 @@
+<?php
+// PHP include() function explained   include() = bring another file here
+include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    this our homepage <br>
+    you can find our stuff here <br>
+</body>
+</html>
+
+<?php 
+include("footer.html");
+?>
