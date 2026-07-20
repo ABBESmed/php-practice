32d7a0be58fd99a278009626980d10f78b076583
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-20 20:03:00 +0200

    PHP and SQL

--- PHP24/index.php
+++ PHP24/index.php
@@ -4,18 +4,28 @@
 // 1. MySQLi Extension
 // 2. PDO (PHP DATA Objects)
 
+
 include("database.php");
 
+$username = "paul";
+$password = "7749bml";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+
+$sql = "INSERT INTO users (user, password)
+        VALUES ('$username', '$hash')";
+
+try{
+    mysqli_query($conn, $sql);
+    echo "user now is registered";
+    
+}
+catch(mysqli_sql_exception){
+    echo "could not register user";
+}
+
+mysqli_close($conn);
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello <br>
-</body>
-</html>
+
+
